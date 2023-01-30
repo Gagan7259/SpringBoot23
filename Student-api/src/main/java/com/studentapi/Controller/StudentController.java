3caa7fd405bdb7d15cfad05cec43331ef0e99c51
Gagan7259 <109654682+Gagan7259@users.noreply.github.com>
2023-01-30 15:41:11 +0530

    client

--- Student-api/src/main/java/com/studentapi/Controller/StudentController.java
+++ Student-api/src/main/java/com/studentapi/Controller/StudentController.java
@@ -1,8 +1,7 @@
 package com.studentapi.Controller;
 
-import com.studentapi.Service.StudentService;
-import com.studentapi.model.Student;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,35 +12,39 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.studentapi.Service.StudentService;
+import com.studentapi.model.Student;
+
+
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class StudentController {
-  @Autowired
-  StudentService services;
+	@Autowired
+	StudentService services;
 
-  // crud api operations
+	// crud api operations
 
-  // Get all students information
-  @GetMapping("/students")
-  public List<Student> getallstudent() {
-    return services.getallstudents();
-  }
+	// Get all students information
+	@GetMapping("/students")
+	public List<Student> getallstudent() {
+		return services.getallstudents();
+	}
 
-  // add students
-  @PostMapping("/student")
-  public void addstudent(@RequestBody Student student) {
-    services.addstudents(student);
-  }
+	// add students
+	@PostMapping("/student")
+	public void addstudent(@RequestBody Student student) {
+		services.addstudents(student);
+	}
+	
+	//update students
+	@PutMapping ("/students")
+	public void updatestudent(@RequestBody Student student) {
+		services.updatestudent(student);
+	}
 
-  //update students
-  @PutMapping("/students")
-  public void updatestudent(@RequestBody Student student) {
-    services.updatestudent(student);
-  }
-
-  // delete Student
-  @DeleteMapping("/student/{id}")
-  public void deletestudent(@PathVariable Integer id) {
-    services.deletestudent(id);
-  }
+	// delete Student
+	@DeleteMapping("/student/{id}")
+	public void deletestudent(@PathVariable Integer id) {
+		services.deletestudent(id);
+	}
 }
